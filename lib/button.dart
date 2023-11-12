56113f28fbe512ed133f2e4bf3e1b8c3df8556b5
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-12 16:36:45 +0300

    update 🆕:button

--- lib/button.dart
+++ lib/button.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
-class SmoothButton extends StatelessWidget {
+class SmoothButton extends StatefulWidget {
+  bool isButtonPressed;
+  Color activeIconColor;
+  Color pasifeIconColor;
 
-  SmoothButton({super.key, });
-  
-  bool isButtonPressed = false;
+  SmoothButton({
+    super.key,
+    this.isButtonPressed = false,
+    this.activeIconColor = Colors.green,
+    this.pasifeIconColor = Colors.red,
+  });
+
+  @override
+  State<SmoothButton> createState() => _SmoothButtonState();
+}
+
+class _SmoothButtonState extends State<SmoothButton> {
   void buttonPressed() {
-    if (isButtonPressed == false) {
-      isButtonPressed = true;
-    }else if (isButtonPressed == true) {
-      isButtonPressed = false;
-    }
+    setState(() {
+      widget.isButtonPressed = !widget.isButtonPressed;
+    });
   }
 
   @override
@@ -21,28 +31,31 @@ class SmoothButton extends StatelessWidget {
         height: 160,
         width: 160,
         decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 63, 7, 132),
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: const [
-              BoxShadow(
-                color: Color.fromARGB(255, 52, 167, 14),
-                offset: Offset(6, 6),
-                blurRadius: 15,
-                spreadRadius: 1,
-              ), // BoxShadow
-              // lighter shadow on top left
-              BoxShadow(
-                color: Colors.white,
-                offset: Offset(-6, -6),
-                blurRadius: 15,
-                spreadRadius: 1,
-                // BoxShadow
-              ),
-            ]),
-        child: const Icon(
+          color: const Color.fromARGB(255, 63, 7, 132),
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 52, 167, 14),
+              offset: Offset(6, 6),
+              blurRadius: 15,
+              spreadRadius: 1,
+            ), // BoxShadow
+            // lighter shadow on top left
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-6, -6),
+              blurRadius: 15,
+              spreadRadius: 1,
+              // BoxShadow
+            ),
+          ],
+        ),
+        child: Icon(
           Icons.favorite,
           size: 60,
-          color: Colors.red,
+          color: widget.isButtonPressed
+              ? widget.activeIconColor
+              : widget.pasifeIconColor,
         ),
       ),
     );
