b35cdb382c31df1551cddfdffdd971803a1c642e
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-12 18:16:10 +0300

    update 🆕: main page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,11 +12,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: Center(
-            child: SmoothButton(
-          
-        )),
+        backgroundColor: Colors.grey,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 70),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SmoothButton(
+                  activeIconColor: Colors.red,
+                  duration: const Duration(milliseconds: 300),
+                  icon: Icons.favorite,
+                  // Use MediaQuery or Int Value
+                  buttonheight: MediaQuery.of(context).size.height * 0.2,
+                  // Use MediaQuery or Int Value
+                  buttonwidth: 300,
+                  borderRadius: 20, iconSize: 60,
+                  text: "Smooth Button",
+                  textStyle: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SmoothButton(
+                  pasifeIconColor: Colors.green,
+                  activeIconColor: Colors.black,
+                  duration: const Duration(milliseconds: 500),
+                  icon: Icons.add_alert,
+                  // Use MediaQuery or Int Value
+                  buttonheight: MediaQuery.of(context).size.height * 0.4,
+                  // Use MediaQuery or Int Value
+                  buttonwidth: 100,
+                  borderRadius: 20, iconSize: 60,
+                ),
+              ]),
+        ),
       ),
     );
   }
